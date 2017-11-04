dfa1df13edc8803dccb7f7829a627d368540aa57
Rémi Verschelde <rverschelde@gmail.com>
2017-11-04 19:14:16 +0100

    Do a commit

--- MySprite.cs
+++ MySprite.cs
@@ -3,20 +3,9 @@ using System;
 
 public class MySprite : Sprite
 {
-    // Member variables here, example:
-    // private int a = 2;
-    // private string b = "textvar";
-
     public override void _Ready()
     {
         GD.print("Hello C# world!");
         
     }
-
-//    public override void _Process(float delta)
-//    {
-//        // Called every frame. Delta is time since last frame.
-//        // Update game logic here.
-//        
-//    }
 }
